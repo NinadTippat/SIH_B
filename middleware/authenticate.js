d3cfa6c8d99653ff3815ea380b960b9ebaaf8cc3
Ninad Tippat <tippatninad17@gmail.com>
2022-10-15 09:39:03 +0530

    commit

---
+++ middleware/authenticate.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/userSchema");
+
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.cookies.access_token;
+    if (!token) {
+      return res.sendStatus(403);
+    }
+    const verifyToken = jwt.verify(token, process.env.KEY);
+
+    const rootUser = await User.findOne({
+      "tokens.token": token,
+    });
+    if (!rootUser) {
+      throw new Error("User not found form jwt");
+    }
+    req.token = token;    
+    next();
+  } catch (err) {
+    res.status(401).send("No user found");
+    console.log(err);
+  }
+};
+
+module.exports = authenticate;
